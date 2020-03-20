1244b08b8c2617d3579897dc101d2f49206de600
William Cutrim da Silva <49375676+WilliamCutrim@users.noreply.github.com>
2020-03-20 02:19:10 -0300

    Building templates and 404/500 pages

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView#, TestView
 
 urlpatterns =[
     path('', IndexView.as_view(), name='index'),
+#    path('teste/', TestView.as_view(), name='teste'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name= 'index.html'
+
+#class TestView(TemplateView):
+#    template_name= '500.html'
