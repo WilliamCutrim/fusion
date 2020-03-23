55803d948b5453afd38a37a60f6e2a5d0a3c8ad4
William Cutrim da Silva <49375676+WilliamCutrim@users.noreply.github.com>
2020-03-23 01:08:43 -0300

    Email form

--- core/views.py
+++ core/views.py
@@ -1,13 +1,30 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView#TemplateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Servico, Funcionario
-class IndexView(TemplateView):
+from .forms import ContatoForm
+#class IndexView(TemplateView):
+class IndexView(FormView):
     template_name= 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
     
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs) #??? Entender melhor essa parte
         context['servicos'] = Servico.objects.order_by('?').all()
         context['funcionarios'] = Funcionario.objects.all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso.')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail.')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+
 #class TestView(TemplateView):
 #    template_name= '500.html'
+    
